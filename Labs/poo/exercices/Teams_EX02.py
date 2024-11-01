b56944dbf62188248dcbfb3d98a45c680cc716ec
ossama argaz <ossama2323>
2024-11-01 12:23:04 +0100

    teams ex 02

--- Labs/poo/exercices/Teams_EX02.py
+++ Labs/poo/exercices/Teams_EX02.py
@@ -5,27 +5,38 @@ class Compte:
     def deposer(self, montant):
         self.solde += int(montant)
     def retrait(self, montant):
-        self.solde -= int(montant)
+        if solde > montant:
+            self.solde -= int(montant)
+        else:
+            print("vous ne pouvez pas car votre sold ne dépasse pas le montant")
+            print("---------------------------------------------------")
     def afficher_solde(self):
         print("votre solde est : " + str(self.solde))
 
 
 
 class CheckingAccount(Compte):
-    def __init__(self,numero_compte,solde, montant):
-        super().__init__(numero_compte, solde, montant)
-        self.frais_transaction = montant * 0.02
-    def retrait(self,solde, montant):
-        self.solde = solde
-        self.solde -= montant + self.frais_transaction
-        return super().retrait(montant)
+    def __init__(self,numero_compte,solde, ft=0.02):
+        super().__init__(numero_compte, solde)
+        self.frais_transaction = ft
+
+    def retrait(self, montant):
+        ftrans = montant * self.frais_transaction
+        if solde > montant:
+            self.solde -= montant + ftrans
+        else:
+            print("vous ne pouvez pas car votre sold ne dépasse pas le montant")
+            print("-----------------------------------------------------")
+    
     def afficher_solde(self):
         print("votre solde avec les frais de transaction est : " + str(self.solde))
+        print("-----------------------------------------------------")
+
 
     
 class SavingsAccount(Compte):
-    def __init__(self,numero_compte, solde, montant, taux_interet):
-        super().__init__(numero_compte, solde, montant)
+    def __init__(self,numero_compte, solde, taux_interet=4):
+        super().__init__(numero_compte, solde)
         self.interet = int(taux_interet) / 100
     def appliquer_interet(self,solde):
         self.solde = solde
@@ -41,24 +52,27 @@ class Client:
         self.comptes = Comptes
     def ouvrir_compte(self,type_compte):
         self.type_compte = type_compte
-        if type_compte == "compte courant":
-            self.comptes.append(CheckingAccount(Numero_compte,solde,montant))
-        elif type_compte == "compte d'épargne":
-            self.comptes.append(SavingsAccount(Numero_compte,solde,montant,taux_interet))
-        else:
-            print("type de compte non valide")
+        for type_compte in self.comptes:
+            numero_compte = input("enter numero de compte")
+            Solde = input("enter numero de compte")
+            if type_compte == "checking":
+                self.comptes.append(CheckingAccount(numero_compte,Solde))
+            elif type_compte == "savings":
+                self.comptes.append(SavingsAccount(Numero_compte,Solde))
+            else:
+                print("type de compte non valide")
+
     def fermer_compte(self,type_compte):
         self.type_compte = type_compte
-        if type_compte == "compte courant":
+        if type_compte == "courant":
             self.comptes.pop()
-        elif type_compte == "compte d'épargne":
+        elif type_compte == "d'épargne":
             self.comptes.pop()
         else:
             print("type de compte non valide")
-    
 
-
-
+    def afficher_comptes(self):
+        print(self.comptes)
 
 
 
@@ -69,15 +83,19 @@ montant = int(input("enter montant"))
 compte.retrait(montant)
 compte.afficher_solde()
 
-checkingAccount = CheckingAccount(Numero_compte, solde, montant)
-checkingAccount.retrait(solde, montant)
+checkingAccount = CheckingAccount(Numero_compte, solde)
+checkingAccount.retrait(montant)
 checkingAccount.afficher_solde()
 
 
 taux_interet = int(input("enter taux interet"))
-savingsAccount = SavingsAccount(Numero_compte, solde, montant, taux_interet)
+savingsAccount = SavingsAccount(Numero_compte, solde, taux_interet)
 savingsAccount.appliquer_interet(solde)
 savingsAccount.afficher_solde()
 
 
 
+client = Client(12,"ossama")
+type_compte = input("enter type de compte a ouvrir")
+client.ouvrir_compte(type_compte)
+client.afficher_comptes()

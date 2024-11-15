e58ebd1d52a363668696a900e7ce2a22caa8a9bc
ossama argaz <ossama2323>
2024-11-15 11:47:43 +0100

    reprndre l'EX05 en classe Client

--- Labs/poo/exercices/Teams_EX05.py
+++ Labs/poo/exercices/Teams_EX05.py
@@ -5,29 +5,23 @@ class Film:
         self._duree_minutes = duree_minutes
     def afficher(self):
         print(f"le filme est {self._titre} et le realisateur est {self._realisateur} et la durée de filme est {self._duree_minutes}")
+f = Film("colombiana", "ossama", 120)
+f.afficher()
 
 class salle():
     def __init__(self, numero_salle, capacite, sieges = []):
         self.numero_salle = numero_salle
         self.capacite = capacite
         self.sieges = sieges
-    def get_sieges(self):
-        return self._sieges
     def afficher_salle(self):
         print(f"le numero de salle est {self.numero_salle} et son capacité {self.capacite}")
     def generer_siege(self):
-        siege = 0
-        for siege in range(self.capacite):
-            if self.capacite > len(self.sieges):
-                self.sieges.append(siege)
-                siege += 1
-            else:
-                print("la salle est pleine")             
+        for i in range(len(self.sieges), self.capacite):
+            siege = Siege(i + 1)
+            self.sieges.append(siege)
+
     def afficher_siege_disponibles(self):
-        if len(self.sieges) < self.capacite:
-            print(f"il y a {self.capacite - len(self.sieges)} siege disponibles")
-        else:
-            print("li n'y a pas de siege disponible")
+            print(f"il y a {len(self.sieges)} siege disponibles")
 
 
 class Siege():
@@ -40,15 +34,12 @@ class Siege():
         else:
             print("le siege est reserve")
     def reserver(self):
-        if self._disponible:
-            self._disponible = False
-            print("le siege est reserver")
+        self.disponible = False
     def liberer(self):
-        if not self._disponible:
-            self._disponible = True
-            print("le siege est disponible")
-
-
+        self.disponible = True
+    
+    def __str__(self):
+        return f" {self.numero_siege} {self.disponible}"
 
 
 class Projection(Film):
@@ -58,31 +49,30 @@ class Projection(Film):
         self._salle = salle
     def afficher_details(self):
         super().afficher() + print(f" et le filme est projecter a la salle numero {self._salle} a {self._heure}", end=" ")
+
     def reserver_siege(self, numero_siege):
         self.numero_siege = numero_siege
-        s = salle()
-        for self.numero_siege in range (len(s._sieges)):
-            if s._sieges[self.numero_siege].disponible == True:
-                s._sieges[self.numero_siege].reserver()
-            else:
-                print(f"le siege {self.numero_siege} est deja reserver")
+        s = salle(2, 50)
+        if s.sieges[self.numero_siege].disponible == True:
+            s.sieges[self.numero_siege].reserver()
+        else:
+            print(f"le siege {self.numero_siege} est deja reserver")
 
     def afficher_siege_reserver(self):
-        for s in range(self._salle.sieges):
-            if not s.disponible:
-                print(f"les siege reserver sont {s.numero_siege}")
+        s = salle(2, 50)
+        for m in s.sieges:
+            if not m.disponible:
+                print(f"les siege reserver sont {m.numero_siege}")
 
 
 class Billet():
     def __init__(self, id_billet, projection, siege, prix):
-        self._id_billet = id_billet
-        self._projection = projection
-        self._siege = siege
-        self._prix = prix
+        self.id_billet = id_billet
+        self.projection = projection
+        self.siege = siege
+        self.prix = prix
     def afficher_billet(self):
-        p = Projection()
-        si = Siege()
-        print(f"le billet numero {self._id_billet} est pour le filme {p.afficher_details} et votre siege est {si._numero_siege} et le prix de billet est {self._prix}")
+        print(f"le billet numero {self.id_billet} est pour le filme {self.projection} et votre siege est {self.siege} et le prix de billet est {self.prix}")
         
 
 class Client():
@@ -90,5 +80,35 @@ class Client():
         self.nom = nom
         self.id_client = id_client
         self.billets = billets
-    def acheter_billet(self,projection, numero_siege, prix):
+    def acheter_billet(self, projection, numero_siege, prix):
+        self.projection = projection
+        self.numero_siege = numero_siege
+        self.prix = prix
+        b = Billet(44, p, 3, 320)
+        billet = Billet(b.id_billet, b.projection, b.siege, b.prix)
+        self.billets.append(billet)
+
+
             
+sa = salle(2, 50)
+sa.afficher_salle()
+sa.generer_siege()
+sa.afficher_siege_disponibles()
+for s in sa.sieges:
+    print(s)
+
+p = Projection("colombiana", "ossama", 120, 2, 7)
+p.reserver_siege(2)
+p.reserver_siege(7)
+p.reserver_siege(4)
+
+for s in sa.sieges:
+    print(s)
+
+p.afficher_siege_reserver()
+
+b = Billet(44, p, 3, 320)
+b.afficher_billet()
+
+c = Client("ossama", 21)
+c.acheter_billet(p, 4, 320)
